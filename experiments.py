237f4ba7dec964245c675654c3cbc2a17ad334ef
talgatomarov <talgat.omarov.1999@gmail.com>
2021-12-06 23:28:59 -0700

    Clean up the experiments

--- experiments.py
+++ experiments.py
@@ -8,13 +8,14 @@ from src.data import AR1Process
 
 root = os.path.dirname(__file__)
 
+
 def experiment1():
     mu, sigma = 100, 10
     percentile = 0.95
     a_candidates = [
         0.001, 0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6,
         0.7, 0.8, 0.9, 0.99, 0.999, 0.9999, 0.99999
-        ]
+    ]
 
     results = {
         "a": a_candidates,
@@ -36,13 +37,15 @@ def experiment1():
             p = data.generate(500, 500)
 
             optimal_model = OptimalTradingModel()
-            wctb_model = WCTBTradingModel(lower_bound=p.min(), upper_bound=p.max())
-            pred_model = PredictionBasedTradingModel(mu, sigma, a, percentile, 500)
+            wctb_model = WCTBTradingModel(
+                lower_bound=p.min(), upper_bound=p.max())
+            pred_model = PredictionBasedTradingModel(
+                mu, sigma, a, percentile, 500)
 
             optimal_yen.append(optimal_model.exchange(p))
             wctb_yen.append(wctb_model.exchange(p))
             pred_yen.append(pred_model.exchange(p))
-        
+
         results['optimal_mu'].append(np.mean(optimal_yen))
         results['optimal_std'].append(np.std(optimal_yen))
         results['wctb_mu'].append(np.mean(wctb_yen))
@@ -57,7 +60,6 @@ def experiment1():
     plt.ylabel("yen")
     plt.title("The amount exchanged yen vs a")
     plt.xscale("logit")
-    # plt.xscale("log")
     plt.legend()
     plt.savefig(os.path.join(root, "output", "experiment2.png"))
     plt.clf()
@@ -75,7 +77,9 @@ def experiment1():
     plt.clf()
 
     results = pd.DataFrame(results)
-    results.to_csv(os.path.join(root, "output", "experiment2.csv"), index=False)
+    results.to_csv(os.path.join(
+        root, "output", "experiment2.csv"), index=False)
+
 
 def experiment2():
     mu, sigma, a = 100, 10, 0.5
@@ -95,7 +99,8 @@ def experiment2():
         for _ in range(n_experiments):
             p = data.generate(500, 500)
 
-            pred_model = PredictionBasedTradingModel(mu, sigma, a, percentile, 500)
+            pred_model = PredictionBasedTradingModel(
+                mu, sigma, a, percentile, 500)
             pred_yen.append(pred_model.exchange(p))
 
         results['prediction_mu'].append(np.mean(pred_yen))
@@ -114,83 +119,18 @@ def experiment2():
     ax[1].set_title("The standard deviation of exchanged yen vs percentile")
     ax[1].set_xscale("logit")
 
-
     plt.savefig(os.path.join(root, "output", "experiment3.png"))
     plt.clf()
 
     results = pd.DataFrame(results)
-    results.to_csv(os.path.join(root, "output", "experiment3.csv"), index=False)
-
-def experiment3():
-    mu, sigma, a = 100, 10, 0.999
-    percentiles = [0.5, 0.6, 0.7, 0.8, 0.9, 0.99, 0.999, 0.9999, 0.99999]
-    n_experiments = 500
-
-    data = AR1Process(mu, sigma, a)
-    optimal_yen, wctb_yen, pred_yen = [], [], []
-
-    results = {
-        "percentile": percentiles,
-        "optimal_mu": [],
-        "wctb_mu": [],
-        "prediction_mu": [],
-        "optimal_std": [],
-        "wctb_std": [],
-        "prediction_std": []
-    }
-
-    for percentile in percentiles:
-        for _ in range(n_experiments):
-            p = data.generate(500, 500)
-
-            optimal_model = OptimalTradingModel()
-            wctb_model = WCTBTradingModel(lower_bound=p.min(), upper_bound=p.max())
-            pred_model = PredictionBasedTradingModel(mu, sigma, a, percentile, 500)
-
-            optimal_yen.append(optimal_model.exchange(p))
-            wctb_yen.append(wctb_model.exchange(p))
-            pred_yen.append(pred_model.exchange(p))
-
-        results['optimal_mu'].append(np.mean(optimal_yen))
-        results['optimal_std'].append(np.std(optimal_yen))
-        results['wctb_mu'].append(np.mean(wctb_yen))
-        results['wctb_std'].append(np.std(wctb_yen))
-        results['prediction_mu'].append(np.mean(pred_yen))
-        results['prediction_std'].append(np.std(pred_yen))
-
-    plt.figure(figsize=(16, 9))
-    plt.plot(results["percentile"], results["optimal_mu"], label="optimal")
-    plt.plot(results["percentile"], results["wctb_mu"], label="wctb")
-    plt.plot(results["percentile"], results["prediction_mu"], label="prediction")
-    plt.xlabel("percentile")
-    plt.ylabel("yen")
-    plt.title("The amount exchanged yen vs percentile")
-    plt.xscale("logit")
-    # plt.xscale("log")
-    plt.legend()
-    plt.savefig(os.path.join(root, "output", "experiment4.png"))
-    plt.clf()
-
-    plt.figure(figsize=(16, 9))
-    plt.plot(results["percentile"], results["optimal_std"], label="optimal")
-    plt.plot(results["percentile"], results["wctb_std"], label="wctb")
-    plt.plot(results["percentile"], results["prediction_std"], label="prediction")
-    plt.xlabel("a")
-    plt.ylabel("yen")
-    plt.title("The standard deviation of exchanged yen vs percentile")
-    plt.xscale("logit")
-    # plt.xscale("log")
-    plt.legend()
-    plt.savefig(os.path.join(root, "output", "experiment4_std.png"))
-    plt.clf()
-
-    results = pd.DataFrame(results)
-    results.to_csv(os.path.join(root, "output", "experiment4.csv"), index=False)
+    results.to_csv(os.path.join(
+        root, "output", "experiment3.csv"), index=False)
 
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("--run", type=str, choices=["all", "1", "2", "3"], default="all")
+    parser.add_argument("--run", type=str,
+                        choices=["all", "1", "2"], default="all")
 
     args = parser.parse_args()
 
@@ -198,8 +138,7 @@ def main():
         experiment1()
     if args.run in ["all", "2"]:
         experiment2()
-    if args.run in ["all", "3"]:
-        experiment3()
+
 
 if __name__ == "__main__":
     main()
